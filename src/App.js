78df5fe7ee345074151d56959155915187058c10
vinay-deshmukh <vinay_deshmukh@outlook.com>
2021-03-29 14:45:03 +0530

    Add basic routing to display different tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Counter from "./pages/counter";
+import { Route, Switch } from "react-router";
+import { Link } from "react-router-dom";
 
 function App() {
   const tasks = [
     {
       text: "Counter",
+      path: "/counter",
+      component: Counter,
     },
     {
       text: "Temperature Counter",
+      path: "/temperature",
     },
     {
       text: "Flight booker",
@@ -31,12 +36,20 @@ function App() {
       <section className="table-contents app-child">
         <ol>
           {tasks.map((task, index) => (
-            <li key={index}>{task.text}</li>
+            <li key={index}>
+              <Link to={task.path}>{task.text}</Link>
+            </li>
           ))}
         </ol>
       </section>
       <section className="task-container app-child">
-        <Counter />
+        <Switch>
+          {tasks.map(({ path, component: Component }, index) => (
+            <Route key={index} path={path}>
+              {Component ? <Component /> : "Work in progress"}
+            </Route>
+          ))}
+        </Switch>
       </section>
     </div>
   );
