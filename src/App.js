211a7592bce479674ea7429465a4ecc0dd76d26c
vinay-deshmukh <vinay_deshmukh@outlook.com>
2021-03-29 14:47:20 +0530

    Move tasks out of App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import Counter from "./pages/counter";
+import Tasks from "./tasks";
 import { Route, Switch } from "react-router";
 import { Link } from "react-router-dom";
 
 function App() {
-  const tasks = [
-    {
-      text: "Counter",
-      path: "/counter",
-      component: Counter,
-    },
-    {
-      text: "Temperature Counter",
-      path: "/temperature",
-    },
-    {
-      text: "Flight booker",
-    },
-    {
-      text: "Timer",
-    },
-    {
-      text: "CRUD",
-    },
-    {
-      text: "Circle Drawer",
-    },
-    {
-      text: "Cells",
-    },
-  ];
   return (
     <div className="App">
       <section className="table-contents app-child">
         <ol>
-          {tasks.map((task, index) => (
+          {Tasks.map((task, index) => (
             <li key={index}>
               <Link to={task.path}>{task.text}</Link>
             </li>
@@ -44,7 +18,7 @@ function App() {
       </section>
       <section className="task-container app-child">
         <Switch>
-          {tasks.map(({ path, component: Component }, index) => (
+          {Tasks.map(({ path, component: Component }, index) => (
             <Route key={index} path={path}>
               {Component ? <Component /> : "Work in progress"}
             </Route>
---
+++ src/tasks.js
@@ -0,0 +1,30 @@
+import Counter from "./pages/counter";
+
+const tasks = [
+  {
+    text: "Counter",
+    path: "/counter",
+    component: Counter,
+  },
+  {
+    text: "Temperature Counter",
+    path: "/temperature",
+  },
+  {
+    text: "Flight booker",
+  },
+  {
+    text: "Timer",
+  },
+  {
+    text: "CRUD",
+  },
+  {
+    text: "Circle Drawer",
+  },
+  {
+    text: "Cells",
+  },
+];
+
+export default tasks;
