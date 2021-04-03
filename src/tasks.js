ca38880117be5526beb3c86cab6cd2ec0d14d128
vinay-deshmukh <vinay_deshmukh@outlook.com>
2021-04-03 15:38:47 +0530

    Create counter docs

--- src/tasks.js
+++ src/tasks.js
@@ -13,24 +13,11 @@ export const tasks = [
     taskDescription: () => (
       <>
         <h2>{`Counter:`}</h2>
-        <h3>
-          {`
-      Implemented Features:
-`}
-        </h3>
-        <p>
-          {`
-
-      1. This
-
-`}
-        </p>
-        {`
-
-
-      Refer here for the original requirements: eugenkiss#counter
-    
-    `}
+        <h3>{`Implemented Features:`}</h3>
+        <p>{"1. A label L that starts at `0`"}</p>
+        <p>{"2. A button that increments the label L by 1."}</p>
+        <p>{"3. A button that decrements the label L by 1."}</p>
+        <p>{"4. A button that resets the label back to 0."}</p>
       </>
     ),
   },
